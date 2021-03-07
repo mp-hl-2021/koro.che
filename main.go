4f3238f829573c8172ef5c9514dcfbe9765bb1c8
Your Name <kisandy@yandex.ru>
2021-03-08 00:47:42 +0300

    init 8 and 9 api

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"time"
 
@@ -9,6 +10,11 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+
+	//8
+	router.HandleFunc("/api/manage/links", getUserLinks).Methods(http.MethodGet)
+	//9
+	router.HandleFunc("/api/manage/stats", getUserLinkStats).Methods(http.MethodGet)
 
 	server := http.Server{
 		Addr:         "localhost:8080",
@@ -20,5 +26,37 @@ func main() {
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func getUserLinks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	o := struct {
+		Links []string `json:"links"`
+	}{
+		Links: []string{"dota2", "lol_kek"},
+	}
+	if err := json.NewEncoder(w).Encode(o); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func getUserLinkStats(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	type linkStat struct {
+		LinkName   string `json:"linkName"`
+		UseCounter int64  `json:"useCounter"`
+	}
+	var dotaStat = linkStat{LinkName: "dota2", UseCounter: 228}
+	var lolKekStat = linkStat{LinkName: "lol_kek", UseCounter: 1337}
+	o := struct {
+		Stats []linkStat `json:"linkStats"`
+	}{
+		Stats: []linkStat{dotaStat, lolKekStat},
+	}
+	if err := json.NewEncoder(w).Encode(o); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }
