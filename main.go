4ad51440f1d54cb3229cba19800721208c836805
Olga Shimanskaia <olgashimanskaia@gmail.com>
2021-03-14 00:29:59 +0300

    Add login, register and logout api requests

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"time"
 
@@ -10,6 +11,9 @@ import (
 func main() {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/api/register", register).Methods(http.MethodPost)
+	router.HandleFunc("/api/login", login).Methods(http.MethodPut)
+	router.HandleFunc("/api/logout", logout).Methods(http.MethodPut)
 	server := http.Server{
 		Addr:         "localhost:8080",
 		ReadTimeout:  10 * time.Second,
@@ -21,4 +25,46 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type registrationModel struct {
+	Login string `json:"login"`
+	Password string `json:"password"`
+}
+
+func register(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	var m registrationModel
+	if err := json.NewDecoder(request.Body).Decode(&m); err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	writer.WriteHeader(http.StatusCreated)
+}
+
+type loginModel struct {
+	Login string `json:"login"`
+	Password string `json:"password"`
+}
+
+func login(writer http.ResponseWriter, request *http.Request) {
+	var m loginModel
+	if err := json.NewDecoder(request.Body).Decode(&m); err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	token := "some_token"
+
+	writer.Header().Set("Content-Type", "application/jwt")
+	writer.Write([]byte(token))
+	writer.WriteHeader(http.StatusOK)
+}
+
+func logout(writer http.ResponseWriter, request *http.Request) {
+	_, err := request.Cookie("_cookie")
+	if err != http.ErrNoCookie {
+		print(err, "Failed to get cookie")
+	}
+	http.Redirect(writer, request, "/", http.StatusFound)
 }
