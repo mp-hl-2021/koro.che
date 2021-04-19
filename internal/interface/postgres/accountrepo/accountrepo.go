e0dbda07f19efe7865948ce5217f625e185cf88f
Ольга Шиманская <ol_shiman@mail.ru>
2021-04-19 16:57:38 +0300

    add functionality for postgres accounts db

--- internal/interface/postgres/accountrepo/accountrepo.go
+++ internal/interface/postgres/accountrepo/accountrepo.go
@@ -14,13 +14,30 @@ func New(conn *sql.DB) *Postgres {
 }
 
 func (p *Postgres) CreateAccount(cred account.Credentials) (account.Account, error) {
-	panic("implement me")
+	aсс := account.Account{Credentials: cred}
+	row := p.conn.QueryRow(`
+		INSERT INTO accounts(login, password) VALUES ($1, $2)
+		RETURNING id`, cred.Login, cred.Password)
+	err := row.Scan(&aсс.Id)
+	return aсс, err
 }
 
 func (p *Postgres) GetAccountById(id string) (account.Account, error) {
-	panic("implement me")
+	aсс := account.Account{}
+	row := p.conn.QueryRow(`
+		SELECT id, login, password
+		FROM accounts
+		WHERE id = $1`, id)
+	err := row.Scan(&aсс.Id, &aсс.Login, &aсс.Password)
+	return aсс, err
 }
 
 func (p *Postgres) GetAccountByLogin(login string) (account.Account, error) {
-	panic("implement me")
+	aсс := account.Account{}
+	row := p.conn.QueryRow(`
+		SELECT id, login, password
+		FROM accounts
+		WHERE login = $1`, login)
+	err := row.Scan(&aсс.Id, &aсс.Login, &aсс.Password)
+	return aсс, err
 }
