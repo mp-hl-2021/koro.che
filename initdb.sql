52d1547433be9f12bfc751805b589d752cb65915
Andrei Kislitsyn <kisandy@yandex.ru>
2021-04-19 13:14:04 +0300

    add initdb

---
+++ initdb.sql
@@ -0,0 +1,19 @@
+create table accounts
+(
+    id       serial primary key,
+    login    varchar(255) not null unique,
+    password varchar(255) not null
+);
+
+create table links
+(
+    id           serial primary key,
+    creator_id   int default null,
+    realLink     varchar(255),
+    shorten_link varchar(255) unique,
+    use_counter  int default 0,
+
+    constraint fk_creator
+        foreign key (creator_id)
+            references accounts (id)
+);
