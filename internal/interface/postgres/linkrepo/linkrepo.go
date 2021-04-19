2fecffe56858f987cd49bb99fb8703a22ad0d3aa
Andrei Kislitsyn <kisandy@yandex.ru>
2021-04-19 17:07:51 +0300

    link logic with postgres

--- internal/interface/postgres/linkrepo/linkrepo.go
+++ internal/interface/postgres/linkrepo/linkrepo.go
@@ -1,6 +1,10 @@
 package linkrepo
 
-import "database/sql"
+import (
+	"database/sql"
+	link2 "koro.che/internal/domain/link"
+	"math/rand"
+)
 
 type Postgres struct {
 	conn *sql.DB
@@ -10,30 +14,130 @@ func New(conn *sql.DB) *Postgres {
 	return &Postgres{conn: conn}
 }
 
+const allowedLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const linkSize = 6
+
+func RandString() string {
+	b := make([]byte, linkSize)
+	for i := range b {
+		b[i] = allowedLetters[rand.Intn(len(allowedLetters))]
+	}
+	return string(b)
+}
+
+type LinkInfo struct {
+	Id         int
+	CreatorId  string
+	RealLink   string
+	Key        string
+	UseCounter int
+}
+
+const queryCreateLink = `
+	insert into 
+	    links(creator_id, real_link, key) 
+	    values ($1, $2, $3)
+`
+
+const queryGetRealLinkByKey = `
+	select real_link from links 
+	where key = $1
+`
+const queryIncreaseLinkStat = `
+	update links
+		set use_counter = use_counter + 1
+	where key = $1
+`
+
+const queryDeleteLink = `
+	delete from links
+	where key = $1
+`
+
+const queryUserLinks = `
+	select key from links
+	where creator_id = $1
+`
+
+const queryLinkStats = `
+	select use_counter from links
+	where key = $1 
+`
+
 func (p *Postgres) CreateShortLink(link string, userId string) (string, error) {
-	panic("implement me")
+	var key string
+	for {
+		key = RandString()
+		row := p.conn.QueryRow(queryGetRealLinkByKey, key)
+		err := row.Scan()
+		if err == sql.ErrNoRows {
+			row := p.conn.QueryRow(queryCreateLink, userId, link, key)
+			err := row.Scan()
+			if err != nil {
+				// todo need wrapping???
+				return "", err
+			}
+			break
+		}
+	}
+	return key, nil
 }
 
 func (p *Postgres) GetLinkByKey(key string) (string, error) {
-	panic("implement me")
+	var realLink string
+	row := p.conn.QueryRow(queryGetRealLinkByKey, key)
+	err := row.Scan(&realLink)
+	if err != nil && err == sql.ErrNoRows {
+		return "", link2.ErrNotExist
+	}
+	return realLink, err
 }
 
 func (p *Postgres) MakeRedirect(key string) (string, error) {
-	panic("implement me")
+	var realLink string
+	row := p.conn.QueryRow(queryGetRealLinkByKey, key)
+	err := row.Scan(&realLink)
+	if err != nil && err == sql.ErrNoRows {
+		return "", link2.ErrNotExist
+	}
+	if err != nil {
+		return "", err // todo wrapping
+	}
+	row = p.conn.QueryRow(queryIncreaseLinkStat, key)
+	err = row.Scan() // todo wrapping?
+
+	return realLink, err
 }
 
 func (p *Postgres) DeleteLink(key string, userId string) (string, error) {
-	panic("implement me")
+	var realLink string
+	row := p.conn.QueryRow(queryGetRealLinkByKey, key)
+	err := row.Scan(&realLink)
+	if err != nil && err == sql.ErrNoRows {
+		return "", link2.ErrNotExist
+	}
+	if err != nil {
+		return "", err // todo wrapping
+	}
+	row = p.conn.QueryRow(queryDeleteLink, key)
+	err = row.Scan() //todo wrap
+	return realLink, err
 }
 
 func (p *Postgres) GetUserLinks(userId string) ([]string, error) {
-	panic("implement me")
+	var userLinks []string
+	row:= p.conn.QueryRow(queryUserLinks, userId)
+	err:= row.Scan(&userLinks) //todo warpping
+	return userLinks, err
 }
 
-func (p *Postgres) GetLinkStat(link string) (uint64, error) {
-	panic("implement me")
+func (p *Postgres) GetLinkStat(key string) (uint64, error) {
+	var stat uint64
+	row:=p.conn.QueryRow(queryLinkStats, key)
+	err := row.Scan(&stat) //todo wrapping
+	return stat, err
 }
 
 func (p *Postgres) CreateUserLinksStorage(userId string) (string, error) {
-	panic("implement me")
+	return "", nil
 }
